9386fda491749b74c070c72d8ea60f02513c481b
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-05-01 16:31:25 +0200

    add typeText function in loader js

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,29 @@
-// Pour la page loader
-// Ce que je souhaite faire : 
-// Le texte s'écrit lettre par lettre
+var selectFirstText = document.querySelector("h1");
+var firstText =  "Hi";
+var contentFirstText = 0;
 
-// Il faut un tableau du texte [ALexanne]
-// une boucle tannt que i < Alexanne alors +1 des lettres
-// Ajouter chaque lettre dans une balise h1 HTML
+var selectSecondText = document.querySelector("h2")
+var secondText = "I'm ALEXANNE"
+var contentSecondText = 0;
 
-// var txt = ['Alexanne'];
-// var i = []
-// for (let i = 0; i < txt.length; i++) {
-//     if(i < txt){
-//         document.querySelector("h1").innerHTML = (i = i + txt[i]);
-//     }
-    
-// }
+var selectThirdText = document.querySelector("p")
+var thirdText = "student in developpement web";
+var contentThirdText = 0;
 
-document.addEventListener('DOMContentLoaded',function(){
-    var txt = [ "Hi" + '<br/>' + "I'm Alexanne"];
-    var p = ["<br>" + "Student web developper"];
-
-      function typeWriter(txt, i) {
-      if (i < (txt.length)) {
-       document.querySelector("h1").innerHTML = txt.substring(0, i+1);
-        setTimeout(function() {
-          typeWriter(txt, i + 1)
-        }, 100);
-      }
-
-        document.querySelector("p").innerHTML = p.substring(0, i+1);
-        setTimeout(function() {
-          typeWriter(p, i + 1)
-        }, 100);
-      
-      }
-    function StartTextAnimation(i) {
-        if (i < txt[i].length) {
-            typeWriter(txt[i], 0);
-           }
+function typeText(){
+    setInterval(() => {
+    if(contentFirstText<firstText.length){
+      selectFirstText.innerHTML += firstText.charAt(contentFirstText);
+        contentFirstText++;
     }
-    StartTextAnimation(0);
-  });
-
+        else if(contentFirstText !== 'undefined' && contentSecondText<secondText.length){
+               selectSecondText.innerHTML += secondText.charAt(contentSecondText);
+               contentSecondText++;
+    }
+    if(contentSecondText == secondText.length && contentThirdText<thirdText.length){
+      selectThirdText.innerHTML += thirdText.charAt(contentThirdText);
+      contentThirdText++;
+    }
+}, 80);
+}
+typeText()
