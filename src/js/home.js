127d4610105c4bfc109544bab59d8243c64a3ac2
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-04-12 18:09:24 +0200

    FIX MenuBg

--- src/js/home.js
+++ src/js/home.js
@@ -1,45 +1,33 @@
 AOS.init({
-	disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-	startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-	initClassName: "aos-init", // class applied after initialization
-	animatedClassName: "aos-animate", // class applied on animation
-	useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
-
-	// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-	offset: 10, // offset (in px) from the original trigger point
-	delay: 200, // values from 0 to 3000, with step 50ms
-	duration: 400, // values from 0 to 3000, with step 50ms
-	easing: "ease", // default easing for AOS animations
-	once: false, // whether animation should happen only once - while scrolling down
-	mirror: false, // whether elements should animate out while scrolling past them
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: true,
+  offset: 10,
+  delay: 200,
+  duration: 400,
+  easing: 'ease',
+  once: false,
+  mirror: false,
 })
 
-let menuBg = document.getElementById("menuBg")
-let menu = document.getElementById("menu")
+const menuBg = document.getElementById('menuBg')
 
-let menuBgOpen = document.querySelector(".header__content--bg--open")
-let menuBgClose = document.querySelector(".header__content--bg--close")
+const menuBgOpen = document.querySelector('.header__content--bg--open')
+const menuBgClose = document.querySelector('.header__content--bg--close')
+const menuContent = document.querySelector('.header__menu')
 
-menuBg.addEventListener("click", () => {
-	if (!menuBg.getAttribute("clicked") || menuBgClose.style.display == "none") {
-		menuBg.classList.toggle("clicked")
-
-		menuBgOpen.style.display = "block"
-		menuBgClose.style.display = "none"
-	}
-	if (!menu.getAttribute("visible") || menuBgClose.style.display == "block") {
-		menu.classList.toggle("visible")
-		menuBgClose.style.display = "block"
-		menuBgOpen.style.display = "none"
-	}
+menuBg.addEventListener('click', () => {
+  const styleBgOpen = getComputedStyle(menuBgOpen)
+  if (styleBgOpen.display === 'block') {
+    menuBgOpen.style.display = 'none'
+    menuBgClose.style.display = 'block'
+    menuContent.classList.toggle('visible')
+  }
+  else {
+    menuBgOpen.style.display = 'block'
+    menuBgClose.style.display = 'none'
+    menuContent.classList.remove('visible')
+  }
 })
-
-const countEl = document.getElementById("count")
-
-function updateVisitCount() {
-	fetch("https://api.countapi.xyz/hit/alexannebrd.netlify.app/?amount=0")
-		.then((res) => res.json())
-		.then((res) => {
-			countEl.innerHTML = res.value
-		})
-}
